ce88011229732559a1785e374c584724d14cefce
azemavdic <asko2212@hotmail.com>
2022-02-20 16:57:51 +0100

    heroku deploy conf

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
@@ -20,5 +21,18 @@ app.use(errorHandler)
 
 //ROUTES
 app.use('/api/events', require('./routes/eventRoutes'))
+
+// Uvezivanje frontenda
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ poruka: 'Astromy photo of the day API' })
+  })
+}
 
 app.listen(PORT, () => console.log(`Server radi na portu ${PORT}`))
